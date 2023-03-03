1b4e9753a6ed8918cab7d8b3004760b2afe3b94f
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2023-03-03 20:55:52 +0700

    test commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 const Login=()=>{
     return(
         <div className="w-full h-screen bg-purple-600">
-            
+            <h1 className="text-2xl text-white text-center">Test</h1>
         </div>
     )
 }
