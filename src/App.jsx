5d472f127183f3ad2d213da7571759a8bfc8047b
Rudfaan Maimahad <84792604+ffaann02@users.noreply.github.com>
2023-03-14 22:09:20 +0700

    fix: missing Game component

--- src/App.jsx
+++ src/App.jsx
@@ -119,6 +119,9 @@ function App() {
           <Route path="/register" element={
             <Register />
           } />
+          <Route path="/game" element={
+            <Game/>
+          } />
           <Route path="/game/drawing" element={
             <DrawingGame />
           } />
